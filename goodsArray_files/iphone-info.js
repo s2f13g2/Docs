130d8cab25a8d0e12f4d26f03cd91c4f68c27def
liuyouth <869689491@qq.com>
2016-01-06 22:23:31 +0800

    w

--- goodsArray_files/iphone-info.js
+++
@@ -1,9 +0,0 @@
-	var array = [
-		{id:"100",name:"iphone6",wangluo:"移动2联通4电信4",neicun:"16GB",wprice:"3500",gprice:"3600",bprice:"3400"},
-		{id:"100",name:"iphone6",wangluo:"移动2联通4电信4",neicun:"64GB",wprice:"3500",gprice:"3600",bprice:"3400"},
-		{id:"100",name:"iphone6",wangluo:"移动2联通4电信4",neicun:"128GB",wprice:"3500",gprice:"3600",bprice:"3400"},
-		{id:"100",name:"iphone6",wangluo:"移动4联通4电信4",neicun:"16GB",wprice:"3500",gprice:"3600",bprice:"3400"},
-		{id:"100",name:"iphone6",wangluo:"移动4联通4电信4",neicun:"64GB",wprice:"3500",gprice:"3600",bprice:"3400"},
-		{id:"100",name:"iphone6",wangluo:"移动4联通4电信4",neicun:"128GB",wprice:"3500",gprice:"3600",bprice:"3400"},
-
-	];
