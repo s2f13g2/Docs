9c4277a6260c11dad35d2f21899479f074904ef4
liuyouth <liubo@liubodeiMac.local>
2017-03-04 01:24:12 +0800

    song

--- music/index.js
+++ music/index.js
@@ -1,26 +1,14 @@
     
 var musicList = [
 
-{
-    title : '欧美男声唱出远距离恋爱的痛苦 Standing In China',
-    singer : '苹果姐',
-    cover  : 'http://al.qn-echo-image-cdn.app-echo.com/c383c0c7bf25d53e7a025c22c4fee7de260902d9?imageMogr2/auto-orient/quality/100%7CimageView2/0/w/100/q/100',
-    src    : 'http://al.qn-echo-cp-cdn.app-echo.com/c2/148b2ae912c6ec2e4c517e8e244f6f0fb7c4d2d9fb463beee152f9686fbcb1797bc7cb1a.mp3?1479956162',
-    lyric  : null
-},
-{
-    title : '有暖气功能的轻盈R&B混音 How It Is (XXYYXX Remix)',
-    singer : '苹果姐',
-    cover  : 'http://up.qn-echo-image-cdn.app-echo.com/FqCrB1R06sm-_RrXzgPIiJqgkeUZ?imageMogr2/auto-orient/quality/100%7CimageView2/0/w/100/q/100',
-    src    : 'http://al.qn-echo-cp-cdn.app-echo.com/c2_96k/1b1a0732391368873a7adbf3f8f599181d681dd4be4b8181bfe3b9612bf506539cf87ef7.mp3?1453952956',
-    lyric  : null
-},
+
+
 { title:"薄荷色的夏天 Summertime Sadness",singer:"wiggler27",cover:"http://kibey-echo.b0.upaiyun.com/poster/2015/03/21/b2d148e3227b5801.png!/fwfh/100x100/unsharp/true",src:"http://ws.qn-echo-cp-cdn.app-echo.com/c2/70c4499cfef204963509c7ac0e36b778785840931b8eed220509d6b44af045d77a8a6353.mp3?1448191088",lyric:"null"},
 {
-    title : '清晨必备醒脑电子串烧',
-    singer : '_Neptune',
-    cover  : 'http://qn.qn-echo-image-cdn.app-echo.com/Fq6MDBt_fVf37jx4OJ1V2aokrz1N?imageMogr2/auto-orient/quality/100%7CimageView2/0/w/100/q/100',
-    src    : 'http://al.qn-echo-cp-cdn.app-echo.com/c2/01c033f65722126c5ecd95903f59cb60a2c01e29ddb65016fe378f69535bf224ba4592b2.mp3?1458711041',
+    title : '贝加尔湖畔',
+    singer : '李建',
+    cover  : '',
+    src    : 'music/song/李健-贝加尔湖畔.mp3',
     lyric  : null
 }
 
@@ -31,7 +19,7 @@ $(function(){
     $('body').show();
     new SMusic({
         musicList : musicList,
-        autoPlay  : true,  //是否自动播放
+        autoPlay  : false,  //是否自动播放
         defaultMode : 1,   //默认播放模式，随机
         callback   : function (obj) {  //返回当前播放歌曲信息
             console.log(obj);
---
+++ music/song.js
@@ -0,0 +1,84 @@
+    
+var musicList = [
+
+
+
+{ title:"薄荷色的夏天 Summertime Sadness",singer:"wiggler27",cover:"http://kibey-echo.b0.upaiyun.com/poster/2015/03/21/b2d148e3227b5801.png!/fwfh/100x100/unsharp/true",src:"http://ws.qn-echo-cp-cdn.app-echo.com/c2/70c4499cfef204963509c7ac0e36b778785840931b8eed220509d6b44af045d77a8a6353.mp3?1448191088",lyric:"null"},
+{
+    title : '贝加尔湖畔',
+    singer : '李建',
+    cover  : '',
+    src    : 'music/song/李健-贝加尔湖畔.mp3',
+    lyric  : null
+},
+{
+    title : '可惜不是你',
+    singer : '曹轩宾',
+    cover  : '',
+    src    : 'music/song/曹轩宾 - 可惜不是你.mp3',
+    lyric  : null
+},
+{
+    title : '天天想你',
+    singer : '陈绮贞',
+    cover  : '',
+    src    : 'music/song/陈绮贞 - 天天想你.mp3',
+    lyric  : null
+},
+{
+    title : '童话镇',
+    singer : '陈一发儿',
+    cover  : '',
+    src    : 'music/song/陈一发儿 - 童话镇.mp3',
+    lyric  : null
+},
+{
+    title : '《Everytime》（翻唱：戴凤鑫&七粒七）',
+    singer : '戴凤鑫',
+    cover  : '',
+    src    : 'music/song/戴凤鑫 - 《Everytime》中文版韩剧《太阳的后裔》OST（翻唱：戴凤鑫&七粒七）.mp3',
+    lyric  : null
+},
+{
+    title : '好久不见',
+    singer : '金玟岐',
+    cover  : '',
+    src    : 'music/song/金玟岐 - 好久不见.mp3',
+    lyric  : null
+},
+{
+    title : '刘哈哈与大先生',
+    singer : '刘心,大鹏',
+    cover  : '',
+    src    : 'music/song/刘心,大鹏 - 刘哈哈与大先生.mp3',
+    lyric  : null
+},
+{
+    title : '她说',
+    singer : '卖血上网a',
+    cover  : '',
+    src    : 'music/song/卖血上网a - 她说.mp3',
+    lyric  : null
+},
+{
+    title : '童话镇',
+    singer : '陈一发儿',
+    cover  : '',
+    src    : 'music/song/陈一发儿 - 童话镇.mp3',
+    lyric  : null
+},
+{
+    title : '大鱼',
+    singer : '双笙',
+    cover  : '',
+    src    : 'music/song/双笙 - 大鱼.mp3',
+    lyric  : null
+},
+
+
+];
+
+
+
+
+
